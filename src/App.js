87e1291f14f718ba0d95203f52c609586b64e383
Reymar Clemente (IS-PH) <reymar_clemente@trendmicro.com>
2020-09-17 11:39:35 +0800

    Responsive

--- src/App.js
+++ src/App.js
@@ -17,28 +17,28 @@ function App() {
   }
 
   return (
-    <div class="main-div">
-      <div class="first-half">
-        <img class="life-code" src="59980531_p0.png" alt="life-code" />
+    <div className="main-div">
+      <div className="first-half">
+        <img className="life-code" src="59980531_p0.png" alt="life-code" />
       </div>
-      <div class="second-half">
-        <span class="calendar-header">My GitHub Calendar</span>
-        <div class="calendar">
+      <div className="second-half">
+        <span className="calendar-header">My GitHub Calendar</span>
+        <div className="calendar">
           Loading the data just for you.
       </div>
-        <span class="intro">Hi, I'm Reymar</span>
-        <p class="about-me">I am a 21-year old software engineer trying to make the cyber world a bit better. How? By
+        <span className="intro">Hi, I'm Reymar</span>
+        <p className="about-me">I am a 21-year old software engineer trying to make the cyber world a bit better. How? By
         contributing to
         open-source projects and being passionate about my career.
         I am an anime and music lover, workaholic, I love games, computer games and I am always looking forward to new
         challenges. Do you have one for me? </p>
 
-        <div class="button-links">
-          <button class="button-email ripple" onClick={mailClick}>
-            <i class="fa fa-envelope"></i>
+        <div className="button-links">
+          <button className="button-email ripple" onClick={mailClick}>
+            <i className="fa fa-envelope"></i>
           &nbsp;EMAIL ME</button>
-          <button class="button-github ripple" onClick={githubClick}>
-            <i class="fa fa-github"></i>
+          <button className="button-github ripple" onClick={githubClick}>
+            <i className="fa fa-github"></i>
           &nbsp;GYAKUSETSU </button>
         </div>
       </div>
